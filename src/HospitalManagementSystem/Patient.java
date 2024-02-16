b6190012a96dd94229272a870cccbf40cb796fd2
Bhabesh463 <bhabeshkrj@gmail.com>
2024-02-17 02:25:10 +0530

    add the required file

---
+++ src/HospitalManagementSystem/Patient.java
@@ -0,0 +1,19 @@
+package HospitalManagementSystem;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class Patient {
+    private Connection connection;
+    private Scanner scanner;
+
+    public Patient(Connection connection, Scanner scanner){
+        this.connection = connection;
+        this.scanner = scanner;
+
+    }
+    public void addPatient(){
+
+    }
+
+}
